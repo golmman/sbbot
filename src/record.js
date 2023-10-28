7ae037f55e520fc2d33c31df4a73bc93a87bf747
Dirk Kretschmann <kretschmanndi@gmail.com>
2023-10-28 23:28:49 +0200

    log results to file

--- src/record.js
+++ src/record.js
@@ -1,24 +1,30 @@
 const axios = require("axios");
+const fs = require("fs");
+
+const SB_STATE_URL = "https://www.saltybet.com/state.json";
+//const SB_STATE_URL = 'https://httpbin.org/get';
 
 async function main() {
   while (true) {
-    let response = await axios.get("https://www.saltybet.com/state.json");
+    let response = await axios.get(SB_STATE_URL);
     let responseData = response.data;
 
-    while (responseData.status === "open") {
+    while (responseData.status !== "locked") {
       await new Promise((r) => setTimeout(r, Math.random() * 1000 + 5000));
-      response = await axios.get("https://www.saltybet.com/state.json");
+      response = await axios.get(SB_STATE_URL);
       responseData = response.data;
     }
 
     while (responseData.status === "locked") {
       await new Promise((r) => setTimeout(r, Math.random() * 1000 + 500));
 
-      response = await axios.get("https://www.saltybet.com/state.json");
+      response = await axios.get(SB_STATE_URL);
       responseData = response.data;
 
       if (responseData.status !== "locked" && responseData !== "open") {
-        console.log(JSON.stringify({ time: new Date(), ...response.data }));
+        const result = JSON.stringify({ time: new Date(), ...response.data });
+        console.log(result);
+        fs.appendFile("./logs/results0.log", result + "\n", () => {});
         await new Promise((r) => setTimeout(r, Math.random() * 1000 + 5000));
       }
     }
