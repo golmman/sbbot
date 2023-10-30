c7dbac3cbb2ae76ab85fe28a95ba4a6f87b0b907
Dirk Kretschmann <kretschmanndi@gmail.com>
2023-10-31 00:24:55 +0100

    improve stats

--- src/stats.js
+++ src/stats.js
@@ -2,6 +2,20 @@ const fs = require('fs');
 
 const SB_RESULT_LOG_FILE = './logs/results0.log';
 
+function getFightersMap(allNames) {
+    const map = new Map();
+    for (const name of allNames) {
+        const fights = map.get(name);
+        if (fights) {
+            map.set(name, fights + 1);
+        } else {
+            map.set(name, 1);
+        }
+    }
+
+    return map;
+}
+
 function main() {
     const resultLogs = fs
         .readFileSync(SB_RESULT_LOG_FILE, 'utf8')
@@ -9,28 +23,39 @@ function main() {
         .filter((s) => s.length > 0)
         .map((l) => JSON.parse(l));
 
-    const resultsWithoutTeamAB = resultLogs.filter(
-        (l) =>
-            l.p1name !== 'Team A' &&
-            l.p1name !== 'Team B' &&
-            l.p2name !== 'Team A' &&
-            l.p2name !== 'Team B',
-    );
-
-    const p1names = resultsWithoutTeamAB.map((l) => l.p1name);
-    const p2names = resultsWithoutTeamAB.map((l) => l.p2name);
+    const p1names = resultLogs.map((l) => l.p1name);
+    const p2names = resultLogs.map((l) => l.p2name);
     const allNames = p1names.concat(p2names);
-    const allNamesUnique = new Set(allNames);
+    const fightersMap = getFightersMap(allNames);
+    const fightersEntries = Array.from(fightersMap.entries());
 
-    const totalFights = resultsWithoutTeamAB.length;
-    const charactersWithMoreThanOneFight = Math.trunc(
-        100 - (100 * allNamesUnique.size) / allNames.length,
+    const fightersEntriesSorted = fightersEntries.sort((a, b) =>
+        a[1] === b[1] ? 0 : a[1] < b[1] ? 1 : -1,
     );
 
-    console.info(`      Total fights without 'Team A/B': ${totalFights}`);
-    console.info(
-        `  Characters with more than one fight: ${charactersWithMoreThanOneFight}%`,
+    let repeatedFighters = 0;
+    for (const value of fightersMap.values()) {
+        if (value > 1) {
+            repeatedFighters += 1;
+        }
+    }
+
+    const totalFighters = fightersEntries.length;
+    const repeatedFightersPercent = Math.trunc(
+        (100 * repeatedFighters) / totalFighters,
     );
+
+    console.info(`     Total fights: ${resultLogs.length}`);
+    console.info(`   Total fighters: ${totalFighters}`);
+    console.info(`Repeated fighters: ${repeatedFightersPercent}%`);
+
+    for (let i = 0; i < 5; i += 1) {
+        console.info(
+            `${fightersEntriesSorted[i][1]
+                .toString()
+                .padStart(4)} fights with ${fightersEntriesSorted[i][0]}`,
+        );
+    }
 }
 
 main();
